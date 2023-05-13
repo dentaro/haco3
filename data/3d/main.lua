f20d89f537a80fc11ab17515fcb2d623293e6e83
dentaro <77978725+dentaro@users.noreply.github.com>
2023-05-13 22:17:06 +0900

    Add files via upload

---
+++ data/3d/main.lua
@@ -0,0 +1,94 @@
+-- 3Dポリゴンの頂点座標
+-- vertices = {
+--     {-16, -16, 0},
+--     {16, -16, 0},
+--     {0, 16, 8},
+--   }
+
+-- vertices={
+--   { 1.0, -1.0, -1.0},
+--   { 1.0, -1.0, 1.0},
+--   {-1.0, -1.0, 1.0},
+--   {-1.0, -1.0, -1.0},
+--   { 1.0, 1.0, -1.0},
+--   { 1.0, 1.0, 1.0},
+--   {-1.0, 1.0, 1.0},
+--   {-1.0, 1.0, -1.0}
+--   }
+
+vertices={
+  { 16, -16, -16},
+  { 16, -16, 16},
+  {-16, -16, 16},
+  {-16, -16, -16},
+  { 16, 16, -16},
+  { 16, 16, 16},
+  {-16, 16, 16},
+  {-16, 16, -16}
+  }
+  
+  -- 描画する3Dポリゴンの回転角度
+angle = 0
+period=24
+frame=0
+crash=0
+
+function _update()
+  frame = frame + 1
+  angle = angle + 0.1
+end
+  
+function _draw()
+  
+  cls()
+
+  -- 3Dポリゴンの座標を計算する
+  local cos = math.cos(angle)
+  local sin = math.sin(angle)
+  local points = {}
+
+  -- テーブルの初期化
+  local index = 1
+
+  for _, vertex in ipairs(vertices) do
+    local x, y, z = vertex[1], vertex[2], vertex[3]
+    local x2 = x * cos - z * sin
+    local z2 = z * cos + x * sin
+    local scale = 64 / (64 - z2) -- パースペクティブ補正
+    local sx = x2 * scale + 64
+    local sy = y * scale + 64
+
+    -- テーブルに座標を追加する
+    points[index] = {}
+    points[index].x = sx
+    points[index].y = sy
+
+    index = index + 1
+  end
+  -- 3Dポリゴンを描画する
+  line(points[1].x, points[1].y, points[2].x, points[2].y, 7)
+  line(points[2].x, points[2].y, points[3].x, points[3].y, 7)
+  line(points[3].x, points[3].y, points[1].x, points[1].y, 7)
+  
+  
+  mv = mid(22,5,33)
+  print(mv,0,0)
+  rv = atan2(3,4)
+  print(rv,0,0)
+end  
+
+-------------------------------------------------------
+function setup()--init
+  _init()
+end
+function loop()--update --draw
+_update()
+_draw()
+end
+
+
+
+
+
+
+
