3a72081bff036a91d5ecb847e8324c70b52335c6
dentaro <77978725+dentaro@users.noreply.github.com>
2025-02-02 12:11:57 +0900

    Add files via upload

--- data/00sample/main.lua
+++ data/00sample/main.lua
@@ -2,7 +2,10 @@ bgcNo = 1
 size = 15
 x=64
 y=60
+
 function setup()
+  debug(1)--デバグ表示
+  music(2,64,60,0,3)
   fillrect(20,20,80,80,7)
 end
 
@@ -11,12 +14,17 @@ function _init()--1回だけ
 end
 
 function _update()--ループします
+  if tpf() == true then
+    x = tp(0)
+    y = tp(1)
+  end
 end
 
 function _draw()--ループします
   cls(bgcNo)
   fillrect(x,y,size,size,8)
   spr8(1,x,y)
+  print("hello world", x, y)
 end
 
 
